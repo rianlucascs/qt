99fbab0156face5e0c31439c8b6aad0dd8cb4155
rianlucascs <rlucasgit@gmail.com>
2024-06-02 23:00:24 -0300

    scr2

--- crqt/scr2.py
+++ crqt/scr2.py
@@ -7,67 +7,54 @@ from os.path import exists
 from os import mkdir
 
 class ListFeatures:
-    def __init__(self, number_of_features_in_a_combination=None):
-        self.number_of_features_in_a_combination = number_of_features_in_a_combination
-        self.path_features_py = f'{path[0]}\\data\\features\\features.py'    
-        self.path_combinations_txt = f'{path[0]}\\data\\features\\combinations\\{number_of_features_in_a_combination}\\combinations.txt'
+    def __init__(self, features=None):
+        if self.check_list(features):
+            self.number_of_features_in_a_combination = len(features[0])
+        else:
+            self.number_of_features_in_a_combination = 1
+        self.path_combinations_txt = f'{path[0]}\\data\\features\\combinations\\{self.number_of_features_in_a_combination}\\combinations.txt'  
 
+    def check_list(self, features): # if list[list]
+        if all(isinstance(elem, list) for elem in features):
+            return True
+        return False
 
-    def reading(self, path):
-        with open(path, 'r', encoding='utf-8') as file:
-            return file.read()
-
-    @property
-    def inventory(self):
-        """Features disponíveis"""
-        file = self.reading(self.path_features_py)
-        number = 0
-        number_of_features = []
-        for row in file.split('\n'):
-            if 'def' in row:
-                number += 1
-                number_of_features.append(int(row.split('(')[0].split(' ')[1].replace('feature', '')))
-        return [number, number_of_features]
-    
-    @property
-    def combinations(self):
-        number = 3
-        number_of_features = [[12, 1], [23, 13], [14, 33]]
-        return [number, number_of_features]
-    
-    def main(self):
-        if self.number_of_features_in_a_combination == 1:
-            return self.inventory
-        
-        elif self.number_of_features_in_a_combination >= 2:
-            return self.combinations
+    def number(self):
+        return self.number_of_features_in_a_combination
 
 class FeaturesManagement:
 
     def __init__(self, ticker, start, end, column='Adj Close', name_model='DecisionTreeClassifier', 
-                 config_model={'max_depth': 3, 'criterion': 'gini'}):
-        self.list_features = ListFeatures()
+                 config_model={'max_depth': 3, 'criterion': 'gini'}, features=[]):
         self.ticker = ticker
         self.start = start
         self.end = end
         self.column = column
         self.name_model = name_model
         self.config_model = config_model
-
+        self.features = features
+        self.list_features = ListFeatures(features).number()
         self.name = ticker.upper().split('.')[0]
-
         self._path_results = f'{path[0]}\\data\\features\\results'
-    
-    def writing(self, path, message):
+
+    def writing_json(self, path, data):
+        with open(path, 'a') as file:
+            dump(data, file, indent=4)
+
+    def reading_json(self, path):
+        with open(path, 'r') as file:
+            return load(file)
+
+    def writing_txt(self, path, message):
         with open(path, 'a', encoding='utf-8') as file:
             file.write(message)
         return None
+    
+    def reading_txt(self, path):
+        with open(path, 'r', encoding='utf-8') as file:
+            return file.read()
 
-    def writing_json(self, path, data):
-        with open(path, 'w') as file:
-            dump(data, file, indent=4)
-            
-    def create_folders(self, number_of_features_in_a_combination):
+    def create_folders(self):
         path = f'{self._path_results}\\{self.name}'
         if not exists(path):
             mkdir(path)
@@ -80,7 +67,7 @@ class FeaturesManagement:
         if not exists(path):
             mkdir(path)
 
-        path = f'{path}\\{number_of_features_in_a_combination}'
+        path = f'{path}\\{self.list_features}' # rabge combination
         if not exists(path):
             mkdir(path)
 
@@ -90,20 +77,33 @@ class FeaturesManagement:
 
         path = f'{_path}\\config_model.json'
         if not exists(path):
-            self.writing_json(mkdir(path), self.config_model)
-        
-        return f'{_path}\\results.json'
+            self.writing_json(path, self.config_model)
+
+        return f'{_path}\\results.txt'
     
-
     def main(self):
-        number_of_features = self.list_features.main()
-        path_results = self.create_folders(self.number_of_features_in_a_combination)
+        path = self.create_folders()
+        try:
+            file = self.reading_txt(path)
+        except FileNotFoundError:
+            pass
 
-        results = {}
-        for f in number_of_features:
-            results[f] = Template(self.ticker, self.start, self.end, [f], self.column, self.name_model, self.config_model).model_result_during_creation[1]
-            self.writing_json(path_results, results)
-
+        for f in self.features:
+            if not all(isinstance(elem, list) for elem in self.features):
+                f = [f]
+            metric = Template(self.ticker, self.start, self.end, f, self.column, self.name_model, self.config_model).model_result_during_creation[1]
+            message = f'%{f}% {metric}\n'
+            try:
+                if not f'%{f}%' in file:
+                    self.writing_txt(path, message)
+            except:
+                self.writing_txt(path, message)
+   
 if __name__ == '__main__':
-    FeaturesManagement('goau4.sa', '2012-05-15', '2022-05-15').main()
+    FeaturesManagement(
+        ticker='azul4.sa', 
+        start='2012-05-15', 
+        end='2022-05-15',
+        features=[[1, 2], [3, 4]]
+        ).main()
 
